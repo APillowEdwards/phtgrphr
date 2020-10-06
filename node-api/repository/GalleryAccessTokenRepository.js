c5fd0ca5dc523c83c51c2dfff6232f539deb5528
Andrew Pillow-Edwards <loophole.edwards@gmail.com>
2020-10-06 14:34:10 +0100

    Delete node api

--- node-api/repository/GalleryAccessTokenRepository.js
+++
@@ -1,9 +0,0 @@
-const {EntityRepository} = require("wetland");
-
-class GalleryAccessTokenRepository extends EntityRepository {
-  findByToken(token) {
-    return this.findOne({token: token}, {populate: ["gallery"]});
-  }
-}
-
-module.exports = GalleryAccessTokenRepository;
